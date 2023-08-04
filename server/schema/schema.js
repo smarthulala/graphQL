9bee53a0363b823362266ac855ab629f5fd55774
smarthulala <chao.long.au@gmail.com>
2023-08-04 23:58:53 +1000

    init

---
+++ server/schema/schema.js
@@ -0,0 +1,31 @@
+const { projects, clients } = require('../sampleData.js')
+
+const { GraphQLObjectType, GraphQLID, GraphQLString } = require('graphql')
+
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: {
+      type: GraphQLID,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    email: {
+      type: GraphQLString,
+    },
+    phone: {
+      type: GraphQLString,
+    },
+  }),
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+    },
+  },
+})
